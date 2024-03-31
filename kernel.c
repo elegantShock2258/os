410bda53d1371b2def9bab272a6131eb40ef69ba
Elegant Shock <elegantShock3307@gmail.com>
2024-04-01 01:09:19 +0530

    cleanup

--- kernel.c
+++ kernel.c
@@ -1,21 +1,12 @@
-#include "./descriptors/GDT/GDT.c"
-#include "./descriptors/IDT/IDT.c"
-#include "./descriptors/IDT/isr.c"
-
+#include "hal/hal.c"
 #include "main.c"
-
 
 #if defined(__linux__)
   #error use i686-elf-as command
 #endif
 
 void init(void){
-  TermInit(); // for term init
-  gdt_init(); // for gdt init
-  idt_init(); // for idt init
-  isr_init(); // for idt init
-  for(size_t i=0; i<VGA_WIDTH;i++) printf("-");
-  printf("\n\n");
+  hal_init();
 }
 
 void kernel_main(void){
--- scripts/runOs.sh
+++ scripts/runOs.sh
@@ -3,6 +3,7 @@ mkdir -p build build/output
 cd build/output
 
 PROJECT_ROOT="../.."
+
 # compile the boot code, kernel.c and link them from the linker
 i686-elf-as $(find $PROJECT_ROOT -type f -name "*.S") -o boot.o
 i686-elf-gcc -c "$PROJECT_ROOT/kernel.c" -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
