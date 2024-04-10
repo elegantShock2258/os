5e8a0d4b764c2e8f43c2947402c74721552eb7c4
Elegant Shock <elegantShock3307@gmail.com>
2024-04-10 23:53:41 +0530

    feat: scrollback

--- main.c
+++ main.c
@@ -5,8 +5,8 @@ void main(void) {
   printf("de"
          "hie %d\n",
          49);
-  // for (int i = 0; i < 100; i++)
-  //   printf("fg\n");
+  for (int i = 0; i < 100; i++)
+    printf("fg %d\n", i);
   printf("%d %d %d", 'K', vga_entry_color(LIGHT_GREY, BLACK), getcolor(0, 0));
 
   for (;;)
