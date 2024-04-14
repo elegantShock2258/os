203af2a6f2faedd448285f68e36ace03e3f89cef
Elegant Shock <elegantShock3307@gmail.com>
2024-04-14 05:36:52 +0530

    feat: pae paging

--- arch/i686/paging/paging.c
+++ arch/i686/paging/paging.c
@@ -1,28 +1,22 @@
 #pragma once
 #include "../utils/kernel_utils.c"
 
-u32 page_directory[1024] __attribute__((aligned(4096)));
-u32 page_dir_ptr_tab[4] __attribute__((aligned(0x20))); // must be aligned to (at least)0x20, ...
-
-extern void loadPageDirectory(unsigned int*);
-extern void enablePaging();
+u64 page_dir_ptr_tab[4]
+    __attribute__((aligned(0x20))); // must be aligned to (at least)0x20, ...
+u64 page_dir[512]
+    __attribute__((aligned(0x1000))); // must be aligned to page boundary
 
 void paging_init() {
-  // initialising page directories
-  for (int i = 0; i < 1024; i++) { // FIXME: init all 1024 pages?
-    u32 page_table[1024] __attribute__((aligned(4096)));
-    // holds the physical address where we want to start mapping these pages
-    // to in this case, we want to map these pages to the very beginning of
-    // memory.
-    // we will fill all 1024 entries in the table, mapping 4 megabytes
-    for (unsigned int k = 0; k < 1024; k++) {
-      // As the address is page aligned, it will always leave 12 bits zeroed.
-      // Those bits are used by the attributes
-      // attributes: supervisor level, read/write, present.
-      page_table[k] = (k * 0x1000) | 3;
-    }
-    page_directory[i] = ((u32)page_table[i]) | 3;
-  }
-  loadPageDirectory(page_directory);
-  enablePaging();
+  page_dir_ptr_tab[0] =
+      (u64)&page_dir |
+      1; // set the page directory into the PDPT and mark it present
+  page_dir[0] = 0b10000011; // Address=0, 2MIB, RW and present
+
+  asm volatile("movl %%cr4, %%eax; bts $5, %%eax; movl %%eax, %%cr4" ::
+                   : "eax"); // set bit5 in CR4 to enable PAE
+  asm volatile("movl %0, %%cr3" ::"r"(&page_dir_ptr_tab)); // load PDPT into CR3
+
+  asm volatile(
+      "movl %%cr0, %%eax; orl $80000000, %%eax; movl %%eax, %%cr0;" ::
+          : "eax");
 }
