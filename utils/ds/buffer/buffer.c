a002345bcc2962dbd27d262b54c63f4e7bb308d3
Ayush Chadha <elegantshock3307@gmail.com>
2024-08-23 19:40:44 +0530

    wip: Circular Buffer

--- utils/ds/buffer/buffer.c
+++ utils/ds/buffer/buffer.c
@@ -1,10 +1,9 @@
 #pragma once
 #include "./buffer.h"
 #include "../../../arch/i686/hal/memory/kheap/kheap.c"
-
 void _cb_init(CircularBuffer *cb, int size) {
-  cb->_cb->head = 0;
-  cb->_cb->tail = 0;
+  cb->_cb->head = -1;
+  cb->_cb->tail = -1;
   cb->_cb->full = false;
   cb->_cb->size = size;
 
@@ -15,29 +14,42 @@ bool _cb_is_empty(const CircularBuffer *cb) {
   return (!cb->_cb->full && (cb->_cb->head == cb->_cb->tail));
 }
 
-bool _cb_is_full(const CircularBuffer *cb) { return cb->_cb->full; }
-
-void _cb_enqueue(CircularBuffer *cb, _CB_TYPE value) {
-  cb->_cb->data[cb->_cb->head] = (void*) &value;
-  if (cb->_cb->full) {
-    cb->_cb->tail = (cb->_cb->tail + 1) % cb->_cb->size;
-  }
-  cb->_cb->head = (cb->_cb->head + 1) % cb->_cb->size;
-  cb->_cb->full = (cb->_cb->head == cb->_cb->tail);
+bool _cb_is_full(const CircularBuffer *cb) {
+  if ((cb->_cb->head == (cb->_cb->tail + 1) % cb->_cb->size) ||
+      (cb->_cb->head == 0 && cb->_cb->tail == cb->_cb->size - 1))
+    return 1;
+  return 0;
 }
 
-bool _cb_dequeue(CircularBuffer *cb, _CB_TYPE *value) {
-  if (_cb_is_empty(cb))
-    return false;
-
-  *value = *(_CB_TYPE *)(cb->_cb->data[cb->_cb->tail]);
-  cb->_cb->full = false;
+void _cb_enqueue(CircularBuffer *cb, _CB_TYPE *value) {
+  if (_cb_is_full(cb)) {
+    // cb->_cb->tail = (cb->_cb->tail + 1) % cb->_cb->size;
+    return;
+  }
+  if (cb->_cb->head == -1)
+    cb->_cb->head = 0;
   cb->_cb->tail = (cb->_cb->tail + 1) % cb->_cb->size;
-  return true;
+  cb->_cb->data[cb->_cb->tail] = (void *)value;
+}
+
+_CB_TYPE _cb_dequeue(CircularBuffer *cb, _CB_TYPE *value) {
+  if (_cb_is_empty(cb))
+    return -1;
+
+  _CB_TYPE val = *(_CB_TYPE *)(cb->_cb->data[cb->_cb->head]);
+  if (cb->_cb->head == cb->_cb->tail) {
+    cb->_cb->head = -1;
+    cb->_cb->tail = -1;
+  } else {
+    cb->_cb->head = (cb->_cb->head + 1) % cb->_cb->size;
+  }
+  // printf("Buffer: INSERT: %d", *(_CB_TYPE*)cb->_cb->data[cb->_cb->tail]);
+  return val;
 }
 
 _CB_TYPE _cb_top(CircularBuffer *cb) {
-  return *(_CB_TYPE *)(cb->_cb->data[cb->_cb->tail]);
+  return *(_CB_TYPE *)(cb->_cb->data[(cb->_cb->tail + cb->_cb->size) %
+                                     cb->_cb->size]);
 }
 void _CB_Constructor(CircularBuffer *state, int size) {
 
@@ -53,3 +65,4 @@ void _CB_Constructor(CircularBuffer *state, int size) {
 
   _cb_init(state, size);
 }
+
