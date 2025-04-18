b6b7a69f2f48079d54db7dd776209b4da15a70dd
Ayush Chadha <elegantshock3307@gmail.com>
2025-04-19 03:24:29 +0530

    husky pre push commit fix

--- arch/i686/hal/interrupts/irq/irq.c
+++ arch/i686/hal/interrupts/irq/irq.c
@@ -26,26 +26,6 @@ void i686_IRQ_Handler(Registers *regs) {
 void IRQ_RegisterHandler(int irq, IRQHandler handler) {
   g_IRQHandlers[irq] = handler;
 }
-u32 ticks = 0;
-void timer(Registers *regs) {
-  ticks++;
-  // 1s = 20 ticks. jus like mc lol
-  // 1000ms = 20 ticks
-  // 1ms = 2/100 ticks
-  // 1ms = 0.02 ticks
-  // 100ms = 2 ticks
-  // 50ms = 1 tick
-  if (ticks == 0xffffffff)
-    ticks = 0;
-}
-
-void sleep(int milliseconds) {
-  u32 initTicks = ticks;
-  u32 ticksToWait = ticks + (0.02 * milliseconds);
-  for (; ticks < ticksToWait;) {
-    asm("hlt");
-  }
-}
 
 void irq_init() {
   PIC_Configure(PIC_REMAP_OFFSET, PIC_REMAP_OFFSET + 8);
