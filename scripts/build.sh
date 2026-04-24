14cfb82617b9291480da65d3b91a15fc8247fb82
elegantshock <elegantshock@gmail.com>
2026-04-24 23:11:12 +0530

    ci: remove matrix and parallel testing, refine build

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,9 +8,9 @@ PROJECT_ROOT="../.."
 
 # compile the boot code, kernel.c and link them from the linker
 i686-elf-as $(find $PROJECT_ROOT -type f -name "*.S") -o boot.o
-i686-elf-gcc -c "$PROJECT_ROOT/kernel.c" -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-i686-elf-gcc -S "$PROJECT_ROOT/kernel.c" -o kernel_asm.S -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-i686-elf-gcc -T "$PROJECT_ROOT/linker.ld" -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+i686-elf-gcc -c "$PROJECT_ROOT/kernel.c" -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra $EXTRA_CFLAGS
+i686-elf-gcc -S "$PROJECT_ROOT/kernel.c" -o kernel_asm.S -std=gnu99 -ffreestanding -O2 -Wall -Wextra $EXTRA_CFLAGS
+i686-elf-gcc -T "$PROJECT_ROOT/linker.ld" -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc $EXTRA_CFLAGS
 
 #  make grub image 
 mkdir -p ./boot/grub
